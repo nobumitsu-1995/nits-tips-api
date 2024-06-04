d1427bcf6ea3dcd59db5f276bfc1827ce55f6514
nobumitsu_masugata <ys.training220208@gmail.com>
2024-06-04 22:59:15 +0900

    feat: reactionStampのcreate repositoryを定義

--- repository/reaction_stamp_repository.go
+++ repository/reaction_stamp_repository.go
@@ -8,6 +8,7 @@ import (
 
 type IReactionStampRepository interface {
 	GetReactionStampsByArticleId(reactionStamp *[]model.ReactionStamp, articleId string) error
+	CreateReactionStamp(reactionStamp *model.ReactionStamp) error
 }
 
 type reactionStampRepository struct {
@@ -20,6 +21,13 @@ func NewReactionStampRepository(db *gorm.DB) IReactionStampRepository {
 
 func (rsr *reactionStampRepository) GetReactionStampsByArticleId(reactionStamps *[]model.ReactionStamp, articleId string) error {
 	if err := rsr.db.Where("article_id=?", articleId).Find(reactionStamps).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (rsr *reactionStampRepository) CreateReactionStamp(reactionStamp *model.ReactionStamp) error {
+	if err := rsr.db.Create(reactionStamp).Error; err != nil {
 		return err
 	}
 	return nil

13c34b4bdf233d66dc9727c5168c7f786e08bc18
nobumitsu_masugata <ys.training220208@gmail.com>
2024-07-28 18:09:04 +0900

    feat: ReactionStampのバリデーションを追加

---
+++ validator/reaction_stamp_validator.go
@@ -0,0 +1,38 @@
+package validator
+
+import (
+	"nits-tips-api/model"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
+type IReactionStampValidator interface {
+	ReactionStampValidator(reactionStamp model.ReactionStamp) error
+}
+
+type reactionStampValidator struct{}
+
+func NewReactionStampValidator() IReactionStampValidator {
+	return &reactionStampValidator{}
+}
+
+func (rsv *reactionStampValidator) ReactionStampValidator(reactionStamp model.ReactionStamp) error {
+	return validation.ValidateStruct(&reactionStamp,
+		validation.Field(
+			&reactionStamp.UserId,
+			validation.Required.Error("UserId is required"),
+		),
+		validation.Field(
+			&reactionStamp.StampId,
+			validation.Required.Error("StampId is required"),
+			validation.Min(1).Error("StampId must be at least 1"),
+			validation.Max(6).Error("StampId must be at most 6"),
+			is.Int.Error("StampId must be an integer"),
+		),
+		validation.Field(
+			&reactionStamp.ArticleId,
+			validation.Required.Error("ArticleId is required"),
+		),
+	)
+}
